f61888fa7132d3bc16b095fb44b97f086bb66a21
Thomas Briggs <thomas.briggs333@gmail.com>
2021-12-16 19:22:39 +0000

    mobile hero

--- frontend-mentor/easybank-landing-page-master/src/js/script.js
+++ frontend-mentor/easybank-landing-page-master/src/js/script.js
@@ -4,4 +4,5 @@ hamburgerMenu.addEventListener("click", () => {
     hamburgerMenu.classList.toggle("open")
     document.getElementById("overlay").classList.toggle("active")
     document.getElementById("mobile-menu").classList.toggle("active")
+    document.querySelector("body").classList.toggle("noscroll")
 })
