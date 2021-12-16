ecb4118030b61006ebacce66a3c12baa3eb40722
Thomas Briggs <thomas.rbiggs333@gmail.com>
2021-12-16 14:17:09 +0000

    Front end

--- frontend-mentor/easybank-landing-page-master/src/js/script.js
+++ frontend-mentor/easybank-landing-page-master/src/js/script.js
@@ -1,1 +1,7 @@
-console.log("Scripts loaded");
+const hamburgerMenu = document.getElementById("hamburger")
+
+hamburgerMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("open")
+    document.getElementById("overlay").classList.toggle("active")
+    document.getElementById("mobile-menu").classList.toggle("active")
+})
